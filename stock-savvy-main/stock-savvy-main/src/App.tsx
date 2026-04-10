e461a7383ce2f396449d9dc5629928a46e83884d
vikram144f-cyber <vikram144f@gmail.com>
2026-04-10 10:18:02 +0530

    Fix router basename for GitHub pages

--- stock-savvy-main/stock-savvy-main/src/App.tsx
+++ stock-savvy-main/stock-savvy-main/src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/stock-manager">
           <InventoryProvider>
             <AppLayout>
               <Routes>
